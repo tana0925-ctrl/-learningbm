69a7bbe817e892b4dc2bf504a016c296df9315f1
tana0925-ctrl <tanachu0925@gmail.com>
2026-06-17 14:12:13 +0900

    Reduce missile speed in typeshoot.js

--- public/typeshoot.js
+++ public/typeshoot.js
@@ -144,7 +144,7 @@
     var dt = Math.min(50, ts - S.last); S.last = ts;
     var f = el('tsField'); var fh = f ? f.clientHeight : 400;
     for (var i = S.missiles.length - 1; i >= 0; i--) {
-      var mo = S.missiles[i]; var speed = 0.12 * dt;
+      var mo = S.missiles[i]; var speed = 0.06 * dt;
       mo.y += mo.dir === 'up' ? -speed : speed;
       mo.node.style.top = mo.y + 'px';
       if (mo.dir === 'up' && mo.y <= 26) { hitCpu(); rm(i, mo); }
